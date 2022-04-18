86619f0b1807b8ea6c8733e51aea3d1bb2eb5240
hendhyhutomo <hendhyhutomo@gmail.com>
2022-04-18 17:18:40 +0700

    🐛 FIX: minor update fix

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,6 +3,7 @@ import { AnimatePresence } from 'framer-motion';
 import { useRouter } from 'next/router';
 import { DefaultSeo } from 'next-seo';
 import SEO from '@/helpers/seo.config';
+import Head from 'next/head';
 import { AppWrapper } from '../context/state.jsx';
 
 export default function App({ Component, pageProps }) {
@@ -11,36 +12,35 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <DefaultSeo {...SEO} />
-
-<Head>
-  {/* FAVICON  */}
-  <link
-    rel="icon"
-    type="image/png"
-    sizes="32x32"
-    href="/favicon-32x32.png"
-  />
-  <link
-    rel="icon"
-    type="image/png"
-    sizes="16x16"
-    href="/favicon-16x16.png"
-  />
-  <link
-    rel="apple-touch-icon"
-    sizes="180x180"
-    href="/apple-touch-icon.png"
-  />
-  <link rel="manifest" href="/site.webmanifest" />
-  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
-  <meta name="theme-color" content="#000000" />
-  <meta name="msapplication-TileColor" content="#000000" />
-  <meta
-    name="viewport"
-    content="width=device-width, initial-scale=1,viewport-fit=cover"
-  />
-  {/* FAVICON  */}
-</Head>
+      <Head>
+        {/* FAVICON  */}
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon-16x16.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
+        />
+        <link rel='manifest' href='/site.webmanifest' />
+        <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#000000' />
+        <meta name='theme-color' content='#000000' />
+        <meta name='msapplication-TileColor' content='#000000' />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1,viewport-fit=cover'
+        />
+        {/* FAVICON  */}
+      </Head>
       <AnimatePresence exitBeforeEnter>
         <AppWrapper>
           <Component {...pageProps} key={router.asPath} />
--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,54 +1,60 @@
-import Layout from "@/components/module/layout";
-import Header from "@/components/module/header";
-import Footer from "@/components/module/footer";
-import Container from "@/components/module/container";
-import React from "react";
-import ContactCard from "@/components/module/contactCard";
+import { useEffect } from 'react';
+import Layout from '@/components/module/layout';
+import Header from '@/components/module/header';
+import Footer from '@/components/module/footer';
+import Container from '@/components/module/container';
+import React from 'react';
+import ContactCard from '@/components/module/contactCard';
+import { useEffectInit } from '@/components/utils/preset';
 
 const contactData = [
   {
     images: [
-      "/contact/contact-1.png",
-      "/contact/contact-1.png",
-      "/contact/contact-1.png",
+      '/contact/contact-1.png',
+      '/contact/contact-1.png',
+      '/contact/contact-1.png',
     ],
-    label: "Head Office",
-    companyName: "PT. Astaguna Wisesa",
-    address: "Jelambar Madya Utama, Jakarta 11460, Indonesia",
-    phone: "021 5657845",
-    email: "info@astaguna.co.id",
-    maps: "https://goo.gl/maps/ozoRK5cx1yWtw3Jq6",
+    label: 'Head Office',
+    companyName: 'PT. Astaguna Wisesa',
+    address: 'Jelambar Madya Utama, Jakarta 11460, Indonesia',
+    phone: '021 5657845',
+    email: 'info@astaguna.co.id',
+    maps: 'https://goo.gl/maps/ozoRK5cx1yWtw3Jq6',
   },
   {
     images: [
-      "/contact/contact-2.png",
-      "/contact/contact-2.png",
-      "/contact/contact-2.png",
+      '/contact/contact-2.png',
+      '/contact/contact-2.png',
+      '/contact/contact-2.png',
     ],
-    label: "Factory",
-    companyName: "PT. Astaguna Wisesa",
-    address: "Kawasan Industri Hyundai BIIE, Cikarang, Bekasi 17550, Indonesia",
+    label: 'Factory',
+    companyName: 'PT. Astaguna Wisesa',
+    address: 'Kawasan Industri Hyundai BIIE, Cikarang, Bekasi 17550, Indonesia',
     phone: null,
     email: null,
-    maps: "https://goo.gl/maps/FGesQVwHy5yYsATp7",
+    maps: 'https://goo.gl/maps/FGesQVwHy5yYsATp7',
   },
 ];
 
+useEffect(() => {
+  useEffectInit();
+}, []);
+
 const Contact = () => {
   return (
-    <Layout className="pt-[86px] lg:pt-32">
-      <Header hamburgerColor="bg-black" />
+    <Layout className='pt-[86px] lg:pt-32'>
+      <Header hamburgerColor='bg-black' />
 
       <Container
-        className="pl-0 pr-0"
-        classNameOuter="px-4 mb-5 md:px-8 md:mb-7 lg:mb-10 xl:px-10 xl:mb-16"
+        className='pl-0 pr-0'
+        classNameOuter='px-4 mb-5 md:px-8 md:mb-7 lg:mb-10 xl:px-10 xl:mb-16'
       >
-        <h1 className="text-morin-blue text-ctitle font-nutmeg font-bold mx-auto mb-8 md:text-ctitleBig lg:text-h2 xl:text-h1 xl:mb-16">
+        <h1 className='text-morin-blue text-ctitle font-nutmeg font-bold mx-auto mb-8 md:text-ctitleBig lg:text-h2 xl:text-h1 xl:mb-16'>
           Contact
         </h1>
 
         {contactData?.map((item) => (
-          <div className="mb-4 last:mb-0" key={item.label}>
+          <div className='mb-4 last:mb-0' key={item.label}>
             <ContactCard
               imageData={item.images}
               label={item.label}
